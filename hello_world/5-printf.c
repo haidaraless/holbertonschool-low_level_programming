625b7949093baec4edcc002fe5ce2392a733260b
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-12 08:55:32 +0000

    Print a message using printf function

---
+++ hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print some text using printf function
+ *
+ * Return: 0 Always
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
