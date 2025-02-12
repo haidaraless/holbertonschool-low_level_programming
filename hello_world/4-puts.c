c0a75b76b47245895d8ad9b5420298948220f9c5
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-12 08:44:25 +0000

    Add description for the code.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
 #include <stdio.h>
+
+/*
+ * main - Prints some text using puts to automaticly add new line and returns 0
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
