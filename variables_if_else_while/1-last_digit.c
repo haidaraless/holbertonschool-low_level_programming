5ea77750f9d90abba8b976bbed9b91be9f516cd1
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-13 08:48:58 +0000

    Test a random last digit number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,17 +7,17 @@
  */
 
 /**
- * main - Prints what ever the last digit of a random number is greater, less, or equals to 5
+ * main - Examin a random number and prints a message accoring to condition
  * Return: 0
  */
 int main(void)
 {
-	int n;
-	int last;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
+
 	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
@@ -26,7 +26,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	else
+	else if (last < 6 && last != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
 	}
