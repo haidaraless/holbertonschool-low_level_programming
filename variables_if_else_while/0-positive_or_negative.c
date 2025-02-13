0b42a3b3ed044762e3cd0ac61612cfeb6e43f1a9
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-13 08:14:18 +0000

    Fix betty style

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/*
+ * File: 0-positive_or_negative.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
 
 /**
- * main - Determine if the randow number is positive of negative
+ * main - prints what ever number is positive of negative
  * Return: 0
  */
 int main(void)
@@ -14,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("Number %d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
-       	else if (n < 0)
-	{
-		printf("Number %d is negative\n", n);
-	}
-       	else
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
