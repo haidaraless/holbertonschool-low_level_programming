5c1572901b9ccb230195363ac29766819019eaa1
Haidar Halessa <haidarhalessa@gmail.com>
2025-02-14 15:02:28 +0000

    prints hexadecemal numbers in lower case.

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+ * File: 8-print_base16.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * main - prints hexdecemal numbers in lower case. 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
