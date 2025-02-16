ffe6e81a9eddd6912350c58944964d40b4d89fa2
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-16 17:56:52 +0000

    print first 50 fibonacci numbers

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/*
+ * File - 102-fibonacci.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * main - prints first 50 fibonacci numbers starting with 1 and 2.
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
