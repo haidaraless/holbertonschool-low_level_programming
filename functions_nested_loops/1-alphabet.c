aaa96283111c00d800641abf0b492be1f1989228
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-16 16:16:23 +0000

    print alphabets in lowercase using a _putchar function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * File - 1-alphabet.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * main - prints alphabets in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
