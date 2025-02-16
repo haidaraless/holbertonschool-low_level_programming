5e0dec3d0f18a4bee4174e84b7f02abff1bd393a
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-16 17:27:05 +0000

    Add a sum function to add two number

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * File - 10-add.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * add - calculate two numbers and return result
+ * @number1: first number
+ * @number2: second number
+ * Return: the result of adding number1 to number2
+ */
+
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
