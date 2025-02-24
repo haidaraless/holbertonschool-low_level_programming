7efdd974c28f18e5375fdfbe0a49f2488a7b8a4d
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-24 08:06:50 +0000

    Add a C program that checks if character is a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * File - 1-isdigit.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * _isdigit - checks if the character is a number
+ * @c: the character being checked
+ * Return: 1 if the character is number, otherwise return 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
