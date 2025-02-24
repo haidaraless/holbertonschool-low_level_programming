60a58da7392081a47fb9058986bebf9dca27d74f
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-24 08:33:20 +0000

    More numbers printing

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * File - 5-more_numbers.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * more_numbers - prints numbers until 14, 10x times followed by new line
+ */
+
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			_putchar(n + '0');
+		}
+
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
