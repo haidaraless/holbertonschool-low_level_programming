b286d454f202638cb0b9f56fed16649ff72f3e6d
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-24 08:13:36 +0000

    Add a mul function to calculate multiplication of two numbers.

---
+++ more_functions_nested_loops/2.mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * File - 2-mul.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * mul - calculate multiplication of two numbers
+ * @a: first number to multiply
+ * @b: second number in multiplication
+ * Return: result if multiplying a by b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
