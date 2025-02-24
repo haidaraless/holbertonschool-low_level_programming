907247f8d5c805c49298145d44a03a27960c099a
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-24 08:20:14 +0000

    Print natural numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * File - 3-print_numbers.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * print_numbers - prints natural numbers followed by new line
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
