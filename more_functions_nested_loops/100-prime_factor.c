7589bd413cdb0312eb4af7707d7889392623acf9
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-25 08:44:53 +0000

    Add 100-prime_factor.c file

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * File - 100-prime_factor.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * main - finds and prints the largest prime factor
+ * of this number: 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	long prime = 612852475143;
+	long division;
+
+	printf("\n");
+
+	return (0);
+}
