8587e8042fb3c59783581691b6a7af679b278da9
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-25 08:02:36 +0000

    Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -20,15 +20,15 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
+		if ((n % 3) == 0 && (n % 5) == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (n % 3)
+		else if ((n % 3) == 0)
 		{
 			printf("Fizz");
 		}
-		else if (n % 5)
+		else if ((n % 5) == 0)
 		{
 			printf("Buzz");
 		}
