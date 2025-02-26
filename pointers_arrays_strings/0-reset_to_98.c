c5345fc821c8158f0f44179e61bbd7f114c89f7c
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-26 12:32:11 +0000

    Reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * File - 0-reset_to_98.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * reset_to_98 - reset a value to 98
+ * @n: is a pointer used to reset value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
