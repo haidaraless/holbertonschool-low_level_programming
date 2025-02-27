5f766599a735c8ccd316cbcb55fe251fa91ac079
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-27 06:27:04 +0000

    prints every character of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/*
+ * File - 6-puts2.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * puts2 - prints every character of a string
+ * @str: a variable that holds string to be print all
+ * its character
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0)
+		{
+			_putchar('\n');
+			break;
+		}
+
+		if (count % 2 == 0)
+			_putchar(str[count]);
+
+		count++;
+	}
+}
