bb6c8776f0675b2676bd7475930a840075c07ffe
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-27 07:14:47 +0000

    Copies string from source to destination

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * File - 9-strcpy.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * *_strcpy - copies string from source to destination
+ * @dest: represents destination to copied string
+ * @src: represents source from string to be copied
+ * Return: a pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
