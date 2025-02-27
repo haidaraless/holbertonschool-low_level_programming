9f5aff196f21e4b77af05e0a104098e8983225d5
Haidar Alessa <haidarhalessa@gmail.com>
2025-02-27 07:46:23 +0000

    Copies string from source to destination

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /*
@@ -12,6 +13,17 @@
  * Return: a pointer to destination
  */
 
+int main(void)
+{
+	char s1[98];
+	char *ptr;
+	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+	printf("%s", s1);
+	printf("%s", ptr);
+
+	return (0);
+}
+
 char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
@@ -21,6 +33,7 @@ char *_strcpy(char *dest, char *src)
 		*(dest + count) = *(src + count);
 		if (*(src + count) == '\0')
 			break;
+		
 		count++;
 	}
 
