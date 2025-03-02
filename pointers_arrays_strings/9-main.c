c5a23429e3f6b2a9bff182f44c96f855083cfcdd
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-02 07:38:22 +0000

    Fix code.

---
+++ pointers_arrays_strings/9-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * File - 9-main.c
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ */
+
+/**
+ * main - checks the code
+ * @s1 a variable to hold string
+ * @ptr a pointer to destination
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char s1[98];
+	char *ptr;
+
+	ptr = _strcpy(s1, "First, solve the problem. Then, wire the code\n");
+	printf("%s", s1);
+	printf("%s", ptr);
+
+	return (0);
+}
