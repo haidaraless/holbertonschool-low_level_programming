f6fdaaca4a7c7c4340bab7ff39bf6e0e92d17050
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-02 07:26:13 +0000

    Fix code.

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,28 +7,11 @@
  */
 
 /**
- * main - checks the code
- * @s1: a variable to hold string
- * @ptr: a pointer to destination
- * Return 0 Always
- *
  * _strcpy - copies string from source to destination
  * @dest: represents destination to copied string
  * @src: represents source from string to be copied
  * Return: a pointer to destination
  */
-
-int main(void)
-{
-	char s1[98];
-	char *ptr;
-
-	ptr = _strcpy(s1, "First, solve the problem. Then, wire the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
-
-	return (0);
-}
 
 char *_strcpy(char *dest, char *src)
 {
