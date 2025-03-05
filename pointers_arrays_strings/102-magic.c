0fe4caadf3d6441f239820c6f41da72438742fc8
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-05 08:08:45 +0000

    102-magic

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -4,10 +4,9 @@ int main(void)
 	int n;
 	int a[5];
 	int *p;
-	
+
 	a[2] = 1024;
 	p = &n;
-
 /*
 * write your line of code here...
 * - you are not allowed to use a
@@ -15,9 +14,10 @@ int main(void)
 * - only one statement. you are not allowed to code anything else than this line of code
 */
 
+
 	*(p + 5) = 98;
 
 	printf("a[2] = %d\n", *(p + 5));
-	
+
 	return (0);
 }
