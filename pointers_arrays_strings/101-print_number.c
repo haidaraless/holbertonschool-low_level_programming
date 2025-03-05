7b484809cb9ea007b46d7b5c742faf22059bfbce
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-05 07:42:37 +0000

    print numbers

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints numbers only
+ * @n: number to be printed
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
