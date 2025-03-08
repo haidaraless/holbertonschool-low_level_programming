81c343739cc1b04f9061770e893b9efcc5cb09cd
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-08 09:47:23 +0000

    Add _putchar prototype

--- recursion/main.h
+++ recursion/main.h
@@ -7,6 +7,7 @@
  * Desc: A Header file contains all prototype functions
  */
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -14,5 +15,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+
 
 #endif
