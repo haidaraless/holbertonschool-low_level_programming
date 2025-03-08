d3f5ea78c7b81093a874bb68f6151b5adfab5115
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-08 09:53:55 +0000

    fix betty style issue

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -16,13 +16,15 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*(s2 + 1) == '\0' || wildcmp(s1, s2 + 1))
 			return (1);
+
 		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 			return (1);
+
 		return (0);
 	}
 
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	
+
 	return (0);
 }
