93d7f6db8040f3cfbd62affdb3b666cb034b2cb3
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-10 19:32:20 +0000

    0-create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,1 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
 
+/**
+ * create_array - creates an array of specefic character
+ * @size: size to be allocated in memory
+ * @c: character to be stored in the array
+ *
+ * Return: a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	/* Return NULL if size is zero */
+	if (size == 0)
+		return (NULL);
+
+	/* Allocate memory for the array */
+	char *arr = (char *)malloc(size * sizeof(char));
+
+	/* Return NULL if memory allocation was fail */
+	if (arr == NULL)
+		return (NULL);
+
+	/* Initialize the array with the specified character */
+	for (int i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ * Desc: A Header file contains all prototype functions
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
