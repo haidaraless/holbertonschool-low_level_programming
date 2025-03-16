641c8d531fb31dce86d5d3b3b7abd9812bf58acd
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-16 19:52:02 +0000

    Create function pointers directory and header file.

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * File: function_pointers.h
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ * Desc: A Header file contains all prototype functions
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
