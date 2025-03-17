c56ecf2ad9cc58f4040f3ba81517190ed8b19838
Haidar Alessa <haidarhalessa@gmail.com>
2025-03-17 07:22:51 +0000

    0-print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,13 @@
 #inclue "function_pointers.h"
 
+/**
+ * print_name - prints the given name using function pointer
+ * @name: the name to be printed
+ * @f: the function pointer used to print the name
+ * Return: void
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-
+	f(name);
 }
