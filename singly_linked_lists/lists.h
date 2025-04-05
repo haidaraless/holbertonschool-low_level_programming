94d22351c89a6ca10983c2a9525cdb3956f957bb
Haidar Alessa <10682@holbertonstudents.com>
2025-04-05 19:15:37 +0000

    0-print_list

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,32 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/*
+ * File: lists.h
+ * Created by: Haidar Alessa 10682@holbertonstudents.com
+ * Desc: A Header file contains all prototype functions
+ */
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function Prototypes */
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
